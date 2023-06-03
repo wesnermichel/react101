13bb7bd2e581b0ef37db0514d1009f64f8fdf6e0
Wesner M <wesnerm@gmail.com>
2023-06-03 16:43:16 -0400

    react 101

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+import { useState } from "react";
+
+function Example(props) {
+  return <h1>{props.cheese}</h1>;
+}
 
 function App() {
+  const [counter, setCounter] = useState({
+    value: 1,
+  });
+
+  console.log("component rendered:", counter);
+
+  const addOne = () => {
+    const newState = { ...counter };
+    newState.value += 1;
+    setCounter(newState);
+    console.log("addOne:", counter);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {counter.value} <br />
+      <button onClick={addOne}> Add One</button>
     </div>
   );
 }
