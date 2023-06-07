1ba2428a278e572818d26d28fccef151fbe27778
Wesner M <wesnerm@gmail.com>
2023-06-07 17:34:22 -0400

    ControlledForm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
  import UncontrolledForm from "./components/UncontrolledForm";
+ import Controlledform from "./components/Controlledform";
  function App() {
 
 
   return (
     <div className="App">
-  < UncontrolledForm />
+  <Controlledform />
     </div>
   );
 }
